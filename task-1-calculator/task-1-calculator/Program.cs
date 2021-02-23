dc54c24a8ef3f885495435345c638fa03e228b2a
Vadim Smirnov <vadimsmirnov01@inbox.ru>
2021-02-24 00:53:03 +0300

    Initial commit.

---
+++ task-1-calculator/task-1-calculator/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace task_1_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Clear();
+                double firstNumber, secondNumber;
+                string deistvie;
+
+                try
+                {
+                    Console.WriteLine("Введите первое число");
+                    firstNumber = double.Parse(Console.ReadLine());
+
+                    Console.WriteLine("Введите второе число");
+                    secondNumber = double.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Вы ввели не число, попробуйте ещё раз!");
+                    Console.ReadLine();
+                    continue;
+                }
+
+                Console.WriteLine("Выберите действие: '+' '-' '*' '/' '^' 'sqrt'");
+                deistvie = Console.ReadLine();
+
+                switch (deistvie)
+                {
+                    case "*":
+                        Console.WriteLine(firstNumber * secondNumber);
+                        break;
+
+                    case "+":
+                        Console.WriteLine(firstNumber + secondNumber);
+                        break;
+
+                    case "-":
+                        Console.WriteLine(firstNumber - secondNumber);
+                        break;
+
+                    case "^":
+                        Console.WriteLine(Math.Pow(firstNumber, secondNumber));
+                        break;
+
+                    case "sqrt":
+                        if (secondNumber == 0)
+                        {
+                            Console.WriteLine("Не верное математическое действие");
+                        }
+                        else
+                        {
+                            Console.WriteLine(Math.Pow(firstNumber, 1 / secondNumber));
+                        }
+                        break;
+
+                    case "/":
+                        if (secondNumber == 0)
+                        {
+                            Console.WriteLine("Не верное математическое действие");
+                        }
+                        else
+                        {
+                            Console.WriteLine(firstNumber / secondNumber);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Ошибка, ввели не то действие");
+                        break;
+                }
+                Console.ReadLine();
+
+
+
+
+
+
+
+
+
+            }
+        }
+    }
+}
